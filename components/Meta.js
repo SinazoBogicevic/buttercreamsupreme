5ef7731ceaa98073f7b8e5b53880f21743f4b95b
SinazoBogicevic <47321511+SinazoBogicevic@users.noreply.github.com>
2021-05-05 22:08:18 +0200

    added social media meta tags

--- components/Meta.js
+++ components/Meta.js
@@ -1,12 +1,18 @@
 import Head from "next/head";
 
-const Meta = ({ title, keywords, description }) => {
+const Meta = ({ title, keywords, description, url, summary, image }) => {
   return (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale= 1" />
       <meta name="keywords" content={keywords} />
-      <meta name="description" content={description} />
+      <meta name="og:description" content={description} />
       <meta name="theme-color" content="#dbc3bf" />
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+      <meta property="fb:app_id" content="your fb app id" />
+      <meta property="og:title" content={title} />
+      <meta name="twitter:card" content={summary} />
+      <meta property="og:image" content={image} />
       <meta charset="utf-8" />
       <link
         rel="apple-touch-icon"
@@ -37,6 +43,9 @@ Meta.defaultProps = {
     "south coast, port edward, margate, uvongo, port shepstone, delivers,bakery near me, cakes near me, cupcakes near me, gluten-free cakes, gluten-free cupcakes",
   description:
     "Buttercream Supreme specialises in gourmet cakes and cupakes. We deliver to the South and Wild Coast",
+  url: "www.buttercreamsupreme.co.za",
+  summary: "creme de la cream of baking",
+  image: "/images/logo.jpg",
 };
 
 export default Meta;
